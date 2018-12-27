8cdfab676a586275e75e29e99f4b74fce8ad1731
Darcie <darciewalsh@hotmail.com>
2018-12-27 12:25:51 +0000

    Product component now using props

--- voting_app/public/js/app.js
+++ voting_app/public/js/app.js
@@ -1,8 +1,17 @@
 class ProductList extends React.Component {
   render() {
+    const product = Seed.products[0];
     return (
       <div className='ui unstackable items'>
-        <Product />
+        <Product
+        id={product.id}
+        title={product.title}
+        description={product.description}
+        url={product.url}
+        votes={product.votes}
+        submitterAvatarUrl={product.submitterAvatarUrl}
+        productImageUrl={product.productImageUrl}
+        />
       </div>
     );
   }
@@ -13,18 +22,28 @@ class Product extends React.Component {
     return (
       <div className='item'>
         <div className='image'>
-          <img src='images/products/image-aqua.png' />
+          <img src={this.props.productImageUrl} />
         </div>
         <div className='middle aligned content'>
+          <div className='header'>
+            <a>
+              <i className='large caret up icon' />
+            </a>
+            {this.props.votes}
+          </div>
           <div className='description'>
-          <a>Fort Knight</a>
-          <p>Authentic renaissance actors, delivered in just two weeks.</p>
+            <a href={this.props.url}>
+              {this.props.title}
+            </a>
+            <p>
+              {this.props.description}
+            </p>
           </div>
           <div className='extra'>
             <span>Submitted by:</span>
             <img
               className='ui avatar image'
-              src='images/avatars/daniel.jpg'
+              src={this.props.submitterAvatarUrl}
             />
           </div>
         </div>
