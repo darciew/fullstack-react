be94fbde095a4d407ebf80af529279ed86dc0d77
Darcie <darciewalsh@hotmail.com>
2018-12-27 12:49:02 +0000

    Rendering multiple products

--- voting_app/public/js/app.js
+++ voting_app/public/js/app.js
@@ -1,17 +1,20 @@
 class ProductList extends React.Component {
   render() {
-    const product = Seed.products[0];
+    const productComponents = Seed.products.map((product) => (
+      <Product
+      key={'product-' + product.id}
+      id={product.id}
+      title={product.title}
+      description={product.description}
+      url={product.url}
+      votes={product.votes}
+      submitterAvatarUrl={product.submitterAvatarUrl}
+      productImageUrl={product.productImageUrl}
+      />
+    ));
     return (
       <div className='ui unstackable items'>
-        <Product
-        id={product.id}
-        title={product.title}
-        description={product.description}
-        url={product.url}
-        votes={product.votes}
-        submitterAvatarUrl={product.submitterAvatarUrl}
-        productImageUrl={product.productImageUrl}
-        />
+        {productComponents}
       </div>
     );
   }
