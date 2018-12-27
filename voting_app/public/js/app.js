b7782919583e5f9a8896917d224ad4f0f12a970d
Darcie <darciewalsh@hotmail.com>
2018-12-27 11:35:13 +0000

    Add ProductList component

--- voting_app/public/js/app.js
+++ voting_app/public/js/app.js
@@ -1,1 +1,14 @@
+class ProductList extends React.Component {
+  render() {
+    return (
+      <div className='ui unstackable items'>
+      Basic React Component.
+      </div>
+    );
+  }
+}
 
+ReactDOM.render(
+  <ProductList />,
+  document.getElementById('content')
+);
