f17664da19deab1348ccca281bc12d7d62783854
Darcie <darciewalsh@hotmail.com>
2019-01-02 17:10:25 +0000

    refactoring ProductList - using property initializers

--- voting_app/public/js/app.js
+++ voting_app/public/js/app.js
@@ -1,19 +1,13 @@
 class ProductList extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
+    state = {
       products: [],
     };
-
-    this.handleProductUpVote = this.handleProductUpVote.bind(this);
-  }
 
   componentDidMount() {
     this.setState({products: Seed.products });
   }
 
-  handleProductUpVote(productId) {
+  handleProductUpVote = (productId) => {
     const nextProducts = this.state.products.map((product) => {
       if (product.id === productId) {
         return Object.assign({}, product, {
@@ -54,15 +48,9 @@ class ProductList extends React.Component {
 }
 
 class Product extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.handleUpVote = this.handleUpVote.bind(this);
-  }
-
-  handleUpVote() {
-    this.props.onVote(this.props.id);
-  }
+  handleUpVote = () => (
+    this.props.onVote(this.props.id)
+  );
 
   render() {
     return (
