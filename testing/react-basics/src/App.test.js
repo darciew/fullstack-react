23aa59768018a5ad6adba9c4e4998e714855d804
Darcie <darciewalsh@hotmail.com>
2019-01-03 14:28:36 +0000

    table should render with a table header of 'Items'

--- testing/react-basics/src/App.test.js
+++ testing/react-basics/src/App.test.js
@@ -1,8 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import React from 'react';
+import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+describe('App', () => {
+  it('should have the `th` "Items"', () => {
+    // shallow render component
+    const wrapper = shallow(<App />);
+    // traverse the virtual DOM, picking out the first `th` element
+    // assert that the element encloses a text value of "Items"
+    expect(wrapper.contains(<th>Items</th>)).toBe(true);
+  });
 });
