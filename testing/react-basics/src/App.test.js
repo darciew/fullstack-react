c9841292bffd3a5ea82e8c3809f54f98e7eaa7db
Darcie <darciewalsh@hotmail.com>
2019-01-03 16:12:38 +0000

    component contains a button element

--- testing/react-basics/src/App.test.js
+++ testing/react-basics/src/App.test.js
@@ -10,4 +10,9 @@ describe('App', () => {
     // assert that the element encloses a text value of "Items"
     expect(wrapper.contains(<th>Items</th>)).toBe(true);
   });
+
+  it('should have a `button` element', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.containsMatchingElement(<button>Add item</button>)).toBe(true);
+  });
 });
