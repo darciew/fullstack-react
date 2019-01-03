b62622909731fb31624a3c5ea47b6e6cd47f7188
Darcie <darciewalsh@hotmail.com>
2019-01-03 19:22:23 +0000

    Refactor - render component before each spec

--- testing/react-basics/src/App.test.js
+++ testing/react-basics/src/App.test.js
@@ -3,27 +3,26 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 describe('App', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  });
+
   it('should have the `th` "Items"', () => {
-    // shallow render component
-    const wrapper = shallow(<App />);
-    // traverse the virtual DOM, picking out the first `th` element
-    // assert that the element encloses a text value of "Items"
     expect(wrapper.contains(<th>Items</th>)).toBe(true);
   });
 
   it('should have a `button` element', () => {
-    const wrapper = shallow(<App />);
     expect(wrapper.containsMatchingElement(<button>Add item</button>)).toBe(true);
   });
 
   it('`button` should be disabled', () => {
-    const wrapper = shallow(<App />);
     const button = wrapper.find('button').first();
     expect(button.props().disabled).toBe(true);
   });
 
   it('should have an `input` element', () => {
-    const wrapper = shallow(<App />);
     expect(wrapper.containsMatchingElement(<input />)).toBe(true);
   });
 });
